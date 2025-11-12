add5a339e447f0587462e13eead6c832f8214ffd
pahmaton <truhlyaev24@gmail.com>
2025-11-12 16:01:02 +0300

    #3 добавил модели

--- ask_pupkin/ask_pupkin/settings.py
+++ ask_pupkin/ask_pupkin/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,6 +120,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
    BASE_DIR / 'static',
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
